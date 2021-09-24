070854d3825cfe684a727c74e1c72934ef9e87f6
fran-gomez <fgvolonterio@gmail.com>
2021-09-23 22:26:02 -0300

    correcciones en formatos y nombres

--- Proyecto/datos.sql
+++ Proyecto/datos.sql
@@ -1,5 +1,5 @@
 USE banco;
-set dateformat dmy;
+#set dateformat dmy;
 #---------------------------------------------------------------------#
 #ELIMINACIONES:
 
@@ -26,39 +26,31 @@ DELETE FROM Deposito;
 DELETE FROM Extraccion;
 DELETE FROM Transferencia;
 
-
-#---------------------------------------------------------------------#
-#INSERCIONES:
-
-#-------------------------Ciudad----------------------------------#
-
 INSERT INTO Ciudad(cod_postal, nombre) 
-VALUES (8000, Bahia Blanca);
+VALUES (8000, 'Bahia Blanca');
 INSERT INTO Ciudad(cod_postal, nombre) 
-VALUES (8300, Neuquen);
+VALUES (8300, 'Neuquen');
 INSERT INTO Ciudad(cod_postal, nombre) 
-VALUES (5500, Mendoza);
+VALUES (5500, 'Mendoza');
 INSERT INTO Ciudad(cod_postal, nombre) 
-VALUES (3300, Posadas);
+VALUES (3300, 'Posadas');
 INSERT INTO Ciudad(cod_postal, nombre)  
-VALUES (9400, Rio Gallegos);
+VALUES (9400, 'Rio Gallegos');
 
-
-#-------------------------Sucursal----------------------------------#
 
 
 INSERT INTO Sucursal(nro_suc, nombre, direccion, telefono, horario, cod_postal) 
-VALUES (001,'Bahia','Estomba 100','123456',8000);
+VALUES (001,'Bahia Blanca','Estomba 100','123456','9.00 a 17.00',8000);
 INSERT INTO Sucursal(nro_suc, nombre, direccion, telefono, horario, cod_postal) 
-VALUES (002,'Nqn','Zelarrayan 100','654321',8300);
+VALUES (002,'Neuquen','Zelarrayan 100','654321','9.00 a 17.00',8300);
 INSERT INTO Sucursal(nro_suc, nombre, direccion, telefono, horario, cod_postal) 
-VALUES (003,'Mdz','Mitre 100','456123',5500);
+VALUES (003,'Mendoza','Mitre 100','456123','9.00 a 17.00',5500);
 INSERT INTO Sucursal(nro_suc, nombre, direccion, telefono, horario, cod_postal) 
-VALUES (004,'Psds','Alvarado 100','321456',3300);
+VALUES (004,'Posadas','Alvarado 100','321456','9.00 a 17.00',3300);
 INSERT INTO Sucursal(nro_suc, nombre, direccion, telefono, horario, cod_postal) 
-VALUES (005,'RioG','Zapiola 100','456456',9400);
+VALUES (005,'Rio Gallegos','Zapiola 100','456456','9.00 a 17.00',9400);
 INSERT INTO Sucursal(nro_suc, nombre, direccion, telefono, horario, cod_postal) 
-VALUES (006,'Bahia','Alsina 100','333456',8000);
+VALUES (006,'Bahia Blanca','Alsina 100','333456','9.00 a 17.00',8000);
 
 #-------------------------Empleado----------------------------------#
 
@@ -86,19 +78,19 @@ VALUES (0006,'Alvarez','Damian','DNI', 43218765, 'Allem 6', '666666', 'administr
 
 
 INSERT INTO Cliente(nro_cliente, apellido, nombre, tipo_doc, nro_doc, direccion, telefono, fecha_nac)
-VALUES (00001, 'Olivarez','Javier','DNI', 28351947, 'Chiclana 121', '123123','18-12-1990');
+VALUES (00001, 'Olivarez','Javier','DNI', 28351947, 'Chiclana 121', '123123','1990-12-18');
 INSERT INTO Cliente(nro_cliente, apellido, nombre, tipo_doc, nro_doc, direccion, telefono, fecha_nac)
-VALUES (00002, 'Galvez','Manuel','DNI', 13351921, 'Mitre 121', '654123','18-06-1992');
+VALUES (00002, 'Galvez','Manuel','DNI', 13351921, 'Mitre 121', '654123','1992-06-18');
 INSERT INTO Cliente(nro_cliente, apellido, nombre, tipo_doc, nro_doc, direccion, telefono, fecha_nac)
-VALUES (00003, 'Hernandez','Guillermo','DNI', 21231947, 'Juan Molina 121', '987223','11-01-1981');
+VALUES (00003, 'Hernandez','Guillermo','DNI', 21231947, 'Juan Molina 121', '987223','1981-11-01');
 
 #-------------------------Plazo_Fijo----------------------------------#
 
 
 INSERT INTO Plazo_Fijo(nro_plazo, capital, fecha_inicio, fecha_fin, tasa_interes, interes, nro_suc)
-VALUES (11111111, 1000.00, '10-06-2021', '10-07-2021', 10.00, 100.00, 001);
+VALUES (11111111, 1000.00, '2021-06-10', '2021-07-10', 10.00, 100.00, 001);
 INSERT INTO Plazo_Fijo(nro_plazo, capital, fecha_inicio, fecha_fin, tasa_interes, interes, nro_suc)
-VALUES (22222222, 100.00, '12-05-2021', '12-06-2021', 10.00, 10.00, 002);
+VALUES (22222222, 100.00, '021-05-12', '2021-06-12', 10.00, 10.00, 002);
 
 
 #-------------------------Tasa_Plazo_Fijo----------------------------------#
@@ -121,15 +113,15 @@ VALUES (22222222, 00002);
 
 
 INSERT INTO Prestamo(nro_prestamo, fecha, cant_meses, monto, tasa_interes, interes, valor_cuota, legajo, nro_cliente) 
-VALUES (12345678, '10-10-2020', 22, 1000.00, 10.00, 100.00, 50.00, 0001, 00002);
+VALUES (12345678, '2020-10-10', 22, 1000.00, 10.00, 100.00, 50.00, 0001, 00002);
 INSERT INTO Prestamo(nro_prestamo, fecha, cant_meses, monto, tasa_interes, interes, valor_cuota, legajo, nro_cliente) 
-VALUES (11115678, '12-02-2021', 10, 2000.00, 10.00, 200.00, 200.00, 0002, 00003);
+VALUES (11115678, '2021-02-12', 10, 2000.00, 10.00, 200.00, 200.00, 0002, 00003);
 
 #-------------------------Pago----------------------------------#
 INSERT INTO Pago(nro_prestamo, nro_pago, fecha_venc, fecha_pago)
-VALUES (12345678, 12, '10-08-2022', '10-09-2021');
+VALUES (12345678, 12, '2022-08-10', '2021-09-10');
 INSERT INTO Pago(nro_prestamo, nro_pago, fecha_venc, fecha_pago)
-VALUES (11115678, 13, '12-12-2021', '12-09-2021');
+VALUES (11115678, 13, '2021-12-12', '2021-09-12');
 
 #-------------------------Tasa_Prestamo---------------------------------#
 
@@ -161,11 +153,11 @@ VALUES (00003, 33345678);
 #-------------------------Tarjeta----------------------------------#
 
 INSERT INTO Tarjeta(nro_tarjeta, PIN, CVT, fecha_venc, nro_cliente, nro_ca)
-VALUES (0123456789123456, md5('1234'), md5('adse'), '23-04-25',00001, 12345678 );
+VALUES (0123456789123456, md5('1234'), md5('adse'), '2025-04-23',00001, 12345678 );
 INSERT INTO Tarjeta(nro_tarjeta, PIN, CVT, fecha_venc, nro_cliente, nro_ca)
-VALUES (1123456789123456, md5('2345'), md5('fdas'), '28-05-22',00002, 22345678 );
+VALUES (1123456789123456, md5('2345'), md5('fdas'), '2022-05-28',00002, 22345678 );
 INSERT INTO Tarjeta(nro_tarjeta, PIN, CVT, fecha_venc, nro_cliente, nro_ca)
-VALUES (2123456789123456, md5('3456'), md5('efds'), '03-08-24',00003, 33345678 );
+VALUES (2123456789123456, md5('3456'), md5('efds'), '2024-08-03',00003, 33345678 );
 
 #-------------------------Caja----------------------------------#
 
@@ -210,11 +202,11 @@ VALUES (62345, 3300, 'Rodriguez 20' );
 
 
 INSERT INTO Transaccion(nro_trans, fecha, hora, monto)
-VALUES (0000000001, '01-02-21', '10:00:00', 1234.00);
+VALUES (0000000001, '2021-01-02', '10:00:00', 1234.00);
 INSERT INTO Transaccion(nro_trans, fecha, hora, monto)
-VALUES (0000000002, '12-04-21', '13:00:00', 14.00);
+VALUES (0000000002, '2021-04-12', '13:00:00', 14.00);
 INSERT INTO Transaccion(nro_trans, fecha, hora, monto)
-VALUES (0000000003, '12-04-21', '13:04:00', 122222.00);
+VALUES (0000000003, '2021-04-12', '13:04:00', 122222.00);
 
 
 #-------------------------Debito----------------------------------#
